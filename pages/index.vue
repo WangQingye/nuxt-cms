4b3ef239d806cd00dd194f9e65e45f86efa19501
WangQingye <329103586@qq.com>
2022-06-05 13:51:04 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Tutorial />
+    <p class="test">11</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+<style lang="scss" scoped>
+$color: red;
+.nuxt-logo {
+  height: 180px;
+}
+.test {
+  color: $--color-primary;
+}
+</style>
