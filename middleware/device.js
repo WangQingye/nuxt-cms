eaa6687910d163fbc6bc25c43e65df11f9c064e8
WangQingye <329103586@qq.com>
2022-09-09 17:49:17 +0800

    主路由到update

--- middleware/device.js
+++ middleware/device.js
@@ -16,7 +16,8 @@ export default function (context) {
     navigator.userAgent;
   context.isMoible = isMoible(context.userAgent);
   if (context.route.path === '/') {
-    context.redirect(context.isMoible ? `/mobile/home` : '/home')
+    context.redirect('/update')
+    // context.redirect(context.isMoible ? `/mobile/home` : '/home')
   }
   // PC转移动
   if (context.isMoible && context.route.path.indexOf('mobile') == -1) {
